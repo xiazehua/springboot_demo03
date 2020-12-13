7ca7507de6697891dfbad97b434aa2249d03e5b7
xiazehua <1101779373@qq.com>
2020-12-13 18:33:42 +0800

    第一次提交

---
+++ project-git/src/com/it/demo/Demo01.java
@@ -0,0 +1,14 @@
+package com.it.demo;
+
+/**
+ * Software：IntelliJ IDEA 2020.1 x64
+ * CassName:      Demo01
+ * Author:         夏泽华
+ * Date:           2020/12/13 17:53
+ * 描述：
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("你好世界");
+    }
+}
