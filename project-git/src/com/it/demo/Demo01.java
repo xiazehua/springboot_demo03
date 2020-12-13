8eb95dc3e69a4d12129dd79d0ea41089e6754add
xiazehua <1101779373@qq.com>
2020-12-13 20:55:58 +0800

    添加新的输出语言

--- project-git/src/com/it/demo/Demo01.java
+++ project-git/src/com/it/demo/Demo01.java
@@ -11,5 +11,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("你好世界");
         System.out.println("你好小伙");
+        System.out.println("还好吗？");
     }
 }
